7b0c5d60edf07f04f2b2e3a34ebe7e939ee8b13a
sawabeshintaro <yandere_erect@yahoo.co.jp>
2020-02-05 13:25:51 +0900

    課題提出 PHP - データ型

---
+++ data Type/textDataType.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>datetype</title>
+</head>
+<body>
+    <p>
+    <?php
+         echo'課題1';
+    ?>
+    </p>
+    <?php
+        $num = "5";
+        echo"りんごが".$num."個あります。";
+    ?>
+    </p>
+    <?php
+        echo'課題2';      
+    ?>
+    </p>
+    <?php
+        echo"string型を使用するときに、ダブルクオート(\")を表示する場合は、バックスラッシュ(\\)が必要です。"
+    ?>
+    </p>
+    <?php
+        echo'課題3';
+    ?>
+    </p>
+    <?php
+        echo'I\'m Security Enginner';
+    ?>
+    </p>
+    <?php
+        echo'課題4';
+    ?>
+    </p>
+    <?php
+        $name = "太郎くん";
+        echo"こんばんは、".$name;
+    ?>
+</body>
+</html>
